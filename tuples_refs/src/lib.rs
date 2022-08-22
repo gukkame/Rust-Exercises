cca5414534185d7a5f6fdbb9a44f093f6ce5cb47
gukka <guka245@gmail.com>
2022-08-22 10:33:16 +0300

    tuple

---
+++ tuples_refs/src/lib.rs
@@ -0,0 +1,38 @@
+
+pub struct Student(i32, String, String);
+pub fn id(student: &Student) -> i32 {
+    let tup = student;
+    tup.0
+
+}
+
+pub fn first_name(student: &Student) -> String {
+    let tup = student;
+    (*tup.1).to_string()
+}
+
+pub fn last_name(student: &Student) -> String {
+    let tup = student;
+    (*tup.2).to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+fn test_id() {
+	let student = Student(20, String::from("Pedro"), String::from("Domingos"));
+	assert_eq!(id(&student), 20);
+}
+#[test]
+fn test_first_name() {
+	let student = Student(20, String::from("Pedro"), String::from("Domingos"));
+	assert_eq!(first_name(&student), "Pedro".to_string());
+}
+#[test]
+fn test_last_name() {
+	let student = Student(20, String::from("Pedro"), String::from("Domingos"));
+	assert_eq!(last_name(&student), "Domingos".to_string());
+}
+}
