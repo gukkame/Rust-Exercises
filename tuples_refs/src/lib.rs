4aa5c70b3784b231110c4ab80a351b6bb86bdd77
gukka <guka245@gmail.com>
2022-08-22 10:34:09 +0300

    tuple

--- tuples_refs/src/lib.rs
+++ tuples_refs/src/lib.rs
@@ -1,9 +1,7 @@
-
-pub struct Student(i32, String, String);
+pub struct Student(pub i32, pub String, pub String);
 pub fn id(student: &Student) -> i32 {
     let tup = student;
     tup.0
-
 }
 
 pub fn first_name(student: &Student) -> String {
@@ -21,18 +19,18 @@ mod tests {
     use super::*;
 
     #[test]
-fn test_id() {
-	let student = Student(20, String::from("Pedro"), String::from("Domingos"));
-	assert_eq!(id(&student), 20);
-}
-#[test]
-fn test_first_name() {
-	let student = Student(20, String::from("Pedro"), String::from("Domingos"));
-	assert_eq!(first_name(&student), "Pedro".to_string());
-}
-#[test]
-fn test_last_name() {
-	let student = Student(20, String::from("Pedro"), String::from("Domingos"));
-	assert_eq!(last_name(&student), "Domingos".to_string());
-}
+    fn test_id() {
+        let student = Student(20, String::from("Pedro"), String::from("Domingos"));
+        assert_eq!(id(&student), 20);
+    }
+    #[test]
+    fn test_first_name() {
+        let student = Student(20, String::from("Pedro"), String::from("Domingos"));
+        assert_eq!(first_name(&student), "Pedro".to_string());
+    }
+    #[test]
+    fn test_last_name() {
+        let student = Student(20, String::from("Pedro"), String::from("Domingos"));
+        assert_eq!(last_name(&student), "Domingos".to_string());
+    }
 }
