acb296958450d9bb11535ea1757337f24db58630
gukka <guka245@gmail.com>
2022-08-22 09:52:32 +0300

    loop

--- looping/src/main.rs
+++ looping/src/main.rs
@@ -1,16 +1,20 @@
 use std::io;
 fn main() {
     let mut i = 1;
+    let mut s = "";
     let something = loop {
         println!("I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?");
         let mut input = String::new();
         io::stdin().read_line(&mut input).expect("Error reading");
-
         if input.trim() == "The letter e" {
-           
-           break i.to_string();
+            break i.to_string();
         }
-        i+=1
+        i += 1
     };
-    println!("It took you {} trial to get the right answer", something);
+    if something.parse::<i32>().unwrap() > 1 {
+        s = "trials";
+    } else {
+        s = "trial";
+    }
+    println!("It took you {} {} to get the right answer", something, s);
 }
