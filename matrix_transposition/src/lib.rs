6e178a175adf75dd87edcdf58a88c9b0e68e2497
gukka <guka245@gmail.com>
2022-08-22 10:16:05 +0300

    matrix

--- matrix_transposition/src/lib.rs
+++ matrix_transposition/src/lib.rs
@@ -1,6 +1,6 @@
 #[derive(Debug)]
 #[derive(PartialEq)]
-pub struct Matrix((i32, i32),(i32, i32));
+pub struct Matrix(pub(i32, i32),(i32, i32));
 pub fn transpose(m: Matrix) -> Matrix {
     let (a, b) = m.0;
     let (c, d) = m.1;
