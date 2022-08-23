b099cdb4a93d4a308b5a77c21da42553fe1634b0
gukka <guka245@gmail.com>
2022-08-23 09:44:02 +0300

    ? at the end

---
+++ doubtful/src/lib.rs
@@ -0,0 +1,16 @@
+pub fn doubtful(s: &mut String) {
+    s.push_str("?")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_function() {
+        let mut s = "hello".to_string();
+        let s_copy = s.clone();
+        doubtful(&mut s);
+        assert_eq!(s, s_copy + "?");
+    }
+}
