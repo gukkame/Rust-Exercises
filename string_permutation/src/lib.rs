5f2230d26437dd0adb81e2a011e51848f7b2f172
gukka <guka245@gmail.com>
2022-08-24 21:02:57 +0300

    bytes

--- string_permutation/src/lib.rs
+++ string_permutation/src/lib.rs
@@ -1,7 +1,9 @@
 pub fn is_permutation(s1: &str, s2: &str) -> bool {
     let vec = s1.as_bytes();
     let vec2 = s2.as_bytes();
-    if vec == vec2 {
+    let sum: u8 = vec.iter().sum();
+    let sum2: u8 = vec2.iter().sum();
+    if sum == sum2 {
         true
     } else {
         false
