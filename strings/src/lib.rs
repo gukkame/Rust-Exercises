a4a456e32930b6e9a892c7c490ad42a3125e23a0
gukka <guka245@gmail.com>
2022-08-24 12:39:40 +0300

    strings

--- strings/src/lib.rs
+++ strings/src/lib.rs
@@ -1,4 +1,18 @@
 pub fn char_length(s: &str) -> usize {
-    s.len()
+    s.chars().count()
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = char_length("hello");
+        let result1 =  char_length("形聲字");
+        let result2 = char_length("😍");
+        assert_eq!(result, 5);
+        assert_eq!(result1, 3);
+        assert_eq!(result2, 1);
+    }
+}
