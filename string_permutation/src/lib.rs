e71d021054b7b81a5391c924e8e80d8cf6303b87
gukka <guka245@gmail.com>
2022-08-24 21:14:45 +0300

    bytes

--- string_permutation/src/lib.rs
+++ string_permutation/src/lib.rs
@@ -1,8 +1,14 @@
 pub fn is_permutation(s1: &str, s2: &str) -> bool {
-    let vec = s1.as_bytes();
-    let vec2 = s2.as_bytes();
-    let sum: u8 = vec.iter().sum();
-    let sum2: u8 = vec2.iter().sum();
+    let vec = s1.bytes();
+    let vec2 = s2.bytes();
+    let mut sum = 0;
+    let mut sum2 = 0;
+    for val in vec {
+        sum += val as i32;
+    }
+    for val in vec2 {
+        sum2 += val as i32;
+    }
     if sum == sum2 {
         true
     } else {
@@ -10,3 +16,22 @@ pub fn is_permutation(s1: &str, s2: &str) -> bool {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn permutation_ascii() {
+        assert!(is_permutation("abcde", "edbca"));
+        assert!(!is_permutation("avcde", "edbca"));
+        assert!(!is_permutation("cde", "edbca"));
+        assert!(is_permutation("code", "deco"));
+        assert!(!is_permutation("code", "deeco"));
+        assert!(!is_permutation("codde", "deeco"));
+    }
+    #[test]
+    fn permutation_unicode() {
+        assert!(is_permutation("hello♥", "♥oelhl"));
+        assert!(!is_permutation("♥", "♥♥"));
+    }
+}
