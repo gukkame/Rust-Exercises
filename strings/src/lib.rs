663e76679933b80a5fcd675bfa601f6bea576d28
gukka <guka245@gmail.com>
2022-08-24 12:36:51 +0300

    strings

--- strings/src/lib.rs
+++ strings/src/lib.rs
@@ -1,14 +1,4 @@
 pub fn char_length(s: &str) -> usize {
-    s.chars().len()
+    s.len()
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
