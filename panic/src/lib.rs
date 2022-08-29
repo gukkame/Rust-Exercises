2b5d27e94e8f9ba63174f8a3f48098e4aa2650d9
gukka <guka245@gmail.com>
2022-08-29 11:15:39 +0300

    file opening error

---
+++ panic/src/lib.rs
@@ -0,0 +1,29 @@
+// use panic::*;
+use std::fs::{self, File};
+
+pub fn open_file(s: &str) -> File {
+    let file_result = File::open(s);
+
+    let file = match file_result {
+        Ok(file) => file,
+        Err(error) => panic!("Problem opening the file: {:?}", error),
+    };
+    file
+    
+}
+#[cfg(test)]
+mod tests {
+	use super::*;
+	#[test]
+	#[should_panic]
+	fn test_opening() {
+		open_file("file.txt");
+	}
+	#[test]
+	fn test_opening_existing() {
+		let filename = "created.txt";
+		File::create(filename).unwrap();
+		open_file(filename);
+		fs::remove_file(filename).unwrap();
+	}
+}
