60af7cb721e7784cc98a2dac6aa59a3fc238bb82
gukka <guka245@gmail.com>
2022-08-31 11:24:04 +0300

    food calorie calculator

--- macro_calculator/src/lib.rs
+++ macro_calculator/src/lib.rs
@@ -1,11 +1,11 @@
 pub use json::*;
 pub struct Food {
-    name: String,
-    calories: [String; 2],
-    proteins: f64,
-    fats: f64,
-    carbs: f64,
-    nbr_of_portions: f64,
+    pub name: String,
+    pub calories: [String; 2],
+    pub proteins: f64,
+    pub fats: f64,
+    pub carbs: f64,
+    pub nbr_of_portions: f64,
 }
 
 pub fn calculate_macros(foods: Vec<Food>) -> json::JsonValue {
@@ -16,20 +16,17 @@ pub fn calculate_macros(foods: Vec<Food>) -> json::JsonValue {
     for meal in foods {
         let cal_num: Vec<&str> = meal.calories[1].split("kcal").collect();
         let one_meal_cal: f64 = cal_num[0].parse().unwrap();
-       cals += meal.nbr_of_portions * one_meal_cal;
-       carbs += meal.nbr_of_portions * meal.carbs;
-       proteins += meal.nbr_of_portions * meal.proteins;
-       fats += meal.nbr_of_portions * meal.fats;
-
+        cals += meal.nbr_of_portions * one_meal_cal;
+        carbs += meal.nbr_of_portions * meal.carbs;
+        proteins += meal.nbr_of_portions * meal.proteins;
+        fats += meal.nbr_of_portions * meal.fats;
     }
     let mut data = json::JsonValue::new_object();
 
-    data["cals"] = ((cals* 100.0).round() / 100.0).into();
-    data["carbs"] = ((carbs* 100.0).round() / 100.0).into();
-    data["proteins"] = ((proteins* 100.0).round() / 100.0).into();
-    data["fats"] = ((fats* 100.0).round() / 100.0).into();
-  
+    data["cals"] = ((cals * 100.0).round() / 100.0).into();
+    data["carbs"] = ((carbs * 100.0).round() / 100.0).into();
+    data["proteins"] = ((proteins * 100.0).round() / 100.0).into();
+    data["fats"] = ((fats * 100.0).round() / 100.0).into();
+
     return data;
 }
-
-
