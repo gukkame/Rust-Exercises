b94f9004e3c2882c415663c7c752be36379a5e47
gukka <guka245@gmail.com>
2022-09-07 13:59:31 +0300

    string changing

---
+++ easy_traits/src/lib.rs
@@ -0,0 +1,34 @@
+#[derive(Clone)]
+struct StringValue {
+    value: String,
+}
+
+trait AppendStr {
+    fn append_str(self, new_str: String) -> Self;
+
+    fn append_number(self, new_number: f64) -> Self;
+
+    fn remove_punctuation_marks(self) -> Self;
+}
+
+impl AppendStr for StringValue {
+    fn append_str(self, new_str: String) -> Self  {
+       let mut res =  self.value.clone();
+       res.push_str(new_str.as_str());
+       StringValue { value: res }
+    }
+
+    fn append_number(self, new_number: f64) -> Self {
+        let mut res =  self.value.clone();
+        res.push_str(new_number.to_string().as_str());
+       StringValue { value: res }
+    }
+
+    fn remove_punctuation_marks(self) -> Self {
+        let res =  self.value.clone();
+       let res2 = res.replace(|c: char| !c.is_ascii(), "");
+       StringValue { value: res2 }
+    }
+}
+
+
